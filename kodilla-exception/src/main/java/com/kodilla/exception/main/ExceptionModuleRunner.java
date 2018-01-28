c1e7bf0a78b52201c8c257521d667c8cf44ef2a6
“michal-adamczyk8 <adamczyk888@gmail.com>
2018-01-28 23:15:27 +0100

    Zadanie 8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -2,6 +2,7 @@ package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
 import com.kodilla.exception.test.FirstChallenge;
+import com.kodilla.exception.test.SecondChallenge;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -19,6 +20,17 @@ public class ExceptionModuleRunner {
             System.out.println("Pojawił się wyjątek: " + e);
         }
         finally {
+           System.out.println("Próbę działania podjęto o " + LocalDateTime.now());
+       }
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+       try {
+           secondChallenge.probablyWillThrowException(0,4);
+       } catch (Exception e){
+           System.out.println("Wystąpił wyjątek: " + e);
+       }
+       finally {
            System.out.println("Próbę działania podjęto o " + LocalDateTime.now());
        }
     }
